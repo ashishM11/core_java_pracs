7bbc2f48e4c35b660f0551054e15df1d098508e5
ashish tiwari <44753015+ashishM11@users.noreply.github.com>
2018-11-07 14:52:52 +0530

    test

--- src/hash_equals_pracs/HashEqualsExample1.java
+++ src/hash_equals_pracs/HashEqualsExample1.java
@@ -3,7 +3,7 @@ package hash_equals_pracs;
 public class HashEqualsExample1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
